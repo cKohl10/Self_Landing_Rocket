00bf31d336df87e72055fd064a11c29ad14afdf3
Carson Kohlbrenner <82614880+cKohl10@users.noreply.github.com>
2024-04-23 13:08:47 -0600

    rod inertia

--- main.jl
+++ main.jl
@@ -14,14 +14,15 @@ include("environment.jl")
 
 # Environment parameters
 x_max = 100.0 # m
-y_max = 100.0 # m
+y_max = 5000.0 # m
 dt = 0.1 # s
 
 # Rocket parameters
 thrust = 10.0 #kN
 torque = 1.0 #kNm
-m = 1000.0 #kg
-I = 10.0 #kg*m^2
+m = 300_000.0 #kg
+h = 50.0 # m height of rocket
+I = (1.0/12.0)*m*(h^2) #kg*m^2 using simple rod model
 
 # Create a 2D rocket environment
 env = RocketEnv2D([0.0, x_max, 0.0, y_max], dt, thrust, torque, m, I)
